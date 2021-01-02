9b0af5834dd76ce58db70f752a1c77948acc944b
David Wu <david204code@gmail.com>
2021-01-02 18:58:45 +0000

    Fix the error of negivating to end and beginning of the array

--- src/Review.js
+++ src/Review.js
@@ -10,17 +10,28 @@ const Review = () => {
   // use index for dynamic rendering
   const {name, job, image, text} = people[index];
 
+  // check for end AND begining of the array
+  const checkNumber = (number) => {
+    if(number > people.length - 1) {
+      return 0;
+    }
+    if(number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
   const nextPerson = () => {
     setIndex((index) => {
       let newIndex = index + 1;
-      return newIndex;
+      return checkNumber(newIndex);
     });
   };
 
   const prevPerson = () => {
     setIndex((index) => {
       let newIndex = index - 1;
-      return newIndex;
+      return checkNumber(newIndex);
     });
   };
 
