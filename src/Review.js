5d865222c0fc08019f68b5c110e30a4b58cd73fe
David Wu <david204code@gmail.com>
2021-01-02 18:51:26 +0000

    Add the functionality of next and previous

--- src/Review.js
+++ src/Review.js
@@ -10,6 +10,20 @@ const Review = () => {
   // use index for dynamic rendering
   const {name, job, image, text} = people[index];
 
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return newIndex;
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return newIndex;
+    });
+  };
+
   return(
     <article className ="review">
       <div className ="img-container">
@@ -24,17 +38,20 @@ const Review = () => {
         <FaQuoteRight />
       </span>
       </div>
+
       <h4 className ="author">{name}</h4>
       <p className ="job">{job}</p>
       <p className ="info">{text}</p>
-      <div className ="button-container">
-        <button className ="prev-btn">
+
+      <div className ="button-container" >
+        <button className ="prev-btn" onClick ={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className ="next-btn">
+        <button className ="next-btn" onClick ={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
+
       <button className ="random-btn">
         Suprise Me
       </button>
